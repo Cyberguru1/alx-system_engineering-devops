fcf51fc1ab1d503d57550effbee5ff34ca230036
Cyberguru1 <hamzacypher34@gmail.com>
2022-11-11 21:39:40 -0600

    bash loops

---
+++ 0x05-processes_and_signals/2-show_your_bash_pid.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#display bash process from ps -u and globbing bash
+# shellcheck disable=SC2009
+ps -u | grep bash
---
+++ 0x05-processes_and_signals/3-show_your_bash_pid_made_easy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# process name with pid
+pgrep -l bash
