23a0600a1cbd194155c69ad844b3339e420eea88
Cyberguru1 <hamzacypher34@gmail.com>
2023-02-10 00:53:39 -0600

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Using what you did in the task #0, extend your 
-Python script to export data in the CSV format.
-"""
+"""Python script to export data in the CSV format."""
 import csv
 import requests
 import sys
