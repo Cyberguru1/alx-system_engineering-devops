e61f8d80df7efaca4ec7e63f654cb4430bf00afd
Cyberguru1 <hamzacypher34@gmail.com>
2023-02-10 00:50:53 -0600

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""returns information about his/her list progress """
+"""Returns information about his/her list progress."""
 import requests
 import sys
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     user = requests.get(url + f"users/{sys.argv[1]}").json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
     completed = [h.get("title") for h in todos if h.get("completed") is True]
-    
+
     print("Employee {} is done with tasks({}/{}:".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(h)) for h in completed]
