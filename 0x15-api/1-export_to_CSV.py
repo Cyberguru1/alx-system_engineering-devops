bc83c54373fa2eaa71d6b10de0fb955ea165c7d8
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-14 08:15:23 -0500

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script to export data in the CSV format."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
@@ -7,12 +7,12 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + f"users/{user_id}").json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-    
-    with open(f"{user_id}.csv", "w", newline="") as f:
-        cursor = csv.writer(f, quoting=csv.QUOTE_ALL)
-        [cursor.writerow(
-            [user_id, username, h.get("completed"), h.get("title")]
-        ) for h in todos]
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
